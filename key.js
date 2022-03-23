16f1085a4a08f88825f83a2a302b9a92f1147908
KazukiSasa <102199141+KazukiSasa@users.noreply.github.com>
2022-03-23 12:48:17 +0900

    Update key.js

--- key.js
+++ key.js
@@ -1,4 +1,4 @@
 
 //SkywayのAPIキーを取得しておく(https://console-webrtc-free.ecl.ntt.com/)
 //作成時に利用するwebサーバのドメインを入力すること
-window.__SKYWAY_KEY__ = '43fde9bc-ca9d-4e22-b63f-41a202bb293d';
+window.__SKYWAY_KEY__ = 'c9bab84f-0377-48fd-a45b-f8410e8a668e';
